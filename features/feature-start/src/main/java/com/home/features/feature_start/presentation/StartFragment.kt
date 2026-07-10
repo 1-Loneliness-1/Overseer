bc8b5d635fd028e6b9f8191d7f2aa63e32dd0906
Kisurin Konstantin <costya.kisurin@yandex.ru>
2026-07-10 11:10:51 +0300

    feat(start_module & presentation): ui state based logic was added for fragment

--- features/feature-start/src/main/java/com/home/features/feature_start/presentation/StartFragment.kt
+++ features/feature-start/src/main/java/com/home/features/feature_start/presentation/StartFragment.kt
@@ -1,13 +1,20 @@
 package com.home.features.feature_start.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import com.home.features.feature_start.R
 import com.home.features.feature_start.databinding.FragmentStartBinding
+import com.home.features.feature_start.presentation.state.StartFragmentUiState
+import java.util.Locale
 
 class StartFragment : Fragment() {
+
+    private val viewModel: StartViewModel
 
     private var _binding: FragmentStartBinding? = null
 
@@ -21,9 +28,70 @@ class StartFragment : Fragment() {
         return _binding?.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        observeViewModel()
+        viewModel.getServerMetrics()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun observeViewModel() {
+        viewModel.uiStateLiveData.observe(viewLifecycleOwner) { fragmentUiState ->
+            when (fragmentUiState) {
+
+                is StartFragmentUiState.Loading -> {
+                    binding.pbMain.isVisible = true
+                    binding.llServerStatus.isVisible = false
+                    binding.clServerMetrics.isVisible = false
+                }
+
+                is StartFragmentUiState.Content -> {
+                    binding.pbMain.isVisible = false
+                    binding.llServerStatus.isVisible = true
+                    binding.clServerMetrics.isVisible = true
+
+                    binding.tvCpuUsageValue.text = String.format(
+                        Locale.getDefault(),
+                        "%d %",
+                        fragmentUiState.serverStatus.cpuUsagePercentage,
+                    )
+                    binding.tvRamUsageValue.text = String.format(
+                        Locale.getDefault(),
+                        "%d %",
+                        fragmentUiState.serverStatus.ramUsagePercentage,
+                    )
+                    binding.tvDiskUsageValue.text = String.format(
+                        Locale.getDefault(),
+                        "%d %",
+                        fragmentUiState.serverStatus.diskUsagePercentage,
+                    )
+                    binding.tvUptimeHoursValue.text = String.format(
+                        Locale.getDefault(),
+                        "%d",
+                        fragmentUiState.serverStatus.uptimeHours
+                    )
+
+                    val colorForVpnStatusBg = ContextCompat.getColor(
+                        requireContext(),
+                        if (fragmentUiState.serverStatus.isVpnRunning) R.color.light_green else R.color.light_red
+                    )
+                    val textForVpnStatusView = getString(
+                        if (fragmentUiState.serverStatus.isVpnRunning) R.string.vpn_is_ok else R.string.vpn_is_down
+                    )
+
+                    binding.tvVpnStatus.apply {
+                        text = textForVpnStatusView
+                        setBackgroundColor(colorForVpnStatusBg)
+                    }
+                }
+
+            }
+        }
     }
 
 }
--- features/feature-start/src/main/java/com/home/features/feature_start/presentation/StartViewModel.kt
+++ features/feature-start/src/main/java/com/home/features/feature_start/presentation/StartViewModel.kt
@@ -1,11 +1,13 @@
 package com.home.features.feature_start.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+
 import com.home.features.feature_start.domain.usecase.GetServerMetricsUseCase
 import com.home.features.feature_start.presentation.state.StartFragmentUiState
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -13,15 +15,15 @@ class StartViewModel @Inject constructor(
     private val serverMetricsUseCase: GetServerMetricsUseCase,
 ) : ViewModel() {
 
-    private val _uiState: MutableStateFlow<StartFragmentUiState> =
-        MutableStateFlow(StartFragmentUiState.Loading)
+    private val _uiStateLiveData: MutableLiveData<StartFragmentUiState> =
+        MutableLiveData(StartFragmentUiState.Loading)
 
-    val uiState: StateFlow<StartFragmentUiState> = _uiState
+    val uiStateLiveData: LiveData<StartFragmentUiState> = _uiStateLiveData
 
     fun getServerMetrics() {
         viewModelScope.launch {
-            _uiState.value = StartFragmentUiState.Loading
-            _uiState.value = serverMetricsUseCase.getServerMetrics()
+            _uiStateLiveData.postValue(StartFragmentUiState.Loading)
+            _uiStateLiveData.postValue(serverMetricsUseCase.getServerMetrics())
         }
     }
 
